51e1f9d151c3f0447789efdfb47552a47c14dada
Ali Salah <ali@salah.io>
2021-02-18 22:57:18 +0200

    add assetPrefix for production

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: isProd ? 'https://out.instatus.com' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
