6bfc62616870cd0cc1744e2d7eb79abda9016c76
Ali Salah <ali@salah.io>
2021-02-20 20:42:27 +0200

    add a loading screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
 import { Provider } from '../lib/context'
+import withLoadingScreen from 'react-loading-screen-hoc'
+import InstatusIcon from '../svg/instatus-icon.svg'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,4 +14,12 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-export default MyApp
+const LoadingScreen = ({ isLoaded }) => (
+  <div className={`fixed top-0 left-0 flex items-center justify-center w-screen h-screen transition duration-300 ease-in-out bg-black ${isLoaded ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'}`}>
+    <InstatusIcon className="w-12 h-12 fill-current text-green-dark" />
+  </div>
+)
+
+export default withLoadingScreen(MyApp, LoadingScreen, {
+  limitMilliSecond: 10 * 1000,
+})
